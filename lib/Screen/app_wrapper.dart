9444c5504a52317d86f8ced95f213853ddf82248
hedi-ben-slimen <mohamedelhedi.benslimene@gmail.com>
2025-12-02 21:25:47 +0100

    added the theme toggle and removeed the duplicated service.dart from git

--- lib/Screen/app_wrapper.dart
+++ lib/Screen/app_wrapper.dart
@@ -3,14 +3,21 @@ import 'countries_screen.dart';
 import 'favourites_screen.dart';
 
 class AppWrapper extends StatefulWidget {
-  const AppWrapper({super.key});
+  final VoidCallback onToggleTheme;
+  final bool isDark;
+
+  const AppWrapper({
+    super.key,
+    required this.onToggleTheme,
+    required this.isDark,
+  });
 
   @override
   State<AppWrapper> createState() => _AppWrapperState();
 }
 
 class _AppWrapperState extends State<AppWrapper> {
-  int _selectedIndex = 0; 
+  int _selectedIndex = 0;
 
   static const List<Widget> _widgetOptions = <Widget>[
     CountriesScreen(),
@@ -26,22 +33,32 @@ class _AppWrapperState extends State<AppWrapper> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+      floatingActionButton: FloatingActionButton(
+        onPressed: widget.onToggleTheme,
+        tooltip: widget.isDark ? 'Switch to light mode' : 'Switch to dark mode',
+        child: Icon(widget.isDark ? Icons.nights_stay : Icons.wb_sunny),
       ),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.public),
-            label: 'Countries',
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Countries'),
           BottomNavigationBarItem(
             icon: Icon(Icons.favorite),
             label: 'Favorites',
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Theme.of(context).primaryColor,
+        // Use white colors for icons/text when in dark mode so they remain visible
+        selectedItemColor: widget.isDark
+            ? Colors.white
+            : Theme.of(context).primaryColor,
+        unselectedItemColor: widget.isDark ? Colors.white70 : Colors.grey,
+        selectedIconTheme: IconThemeData(
+          color: widget.isDark ? Colors.white : Theme.of(context).primaryColor,
+        ),
+        unselectedIconTheme: IconThemeData(
+          color: widget.isDark ? Colors.white70 : Colors.grey,
+        ),
         onTap: _onItemTapped,
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,38 @@ void main() {
   runApp(
     MultiProvider(
       providers: [
-        // SM-01: Implement Provider to manage your app state
+        // App state
         ChangeNotifierProvider(
-          // Injecting ApiService into the CountryProvider constructor
           create: (_) => CountryProvider(apiService: ApiService()),
         ),
       ],
-      child: const CountryExplorerApp(),
+      child: CountryExplorerApp(),
     ),
   );
 }
 
-class CountryExplorerApp extends StatelessWidget {
+class CountryExplorerApp extends StatefulWidget {
   const CountryExplorerApp({super.key});
 
   @override
+  State<CountryExplorerApp> createState() => _CountryExplorerAppState();
+}
+
+class _CountryExplorerAppState extends State<CountryExplorerApp> {
+  ThemeMode _mode = ThemeMode.light;
+
+  void _toggleTheme() {
+    setState(() {
+      _mode = _mode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // MS-01: Basic app structure with "Country Explorer" title
     return MaterialApp(
       title: 'Country Explorer',
       theme: ThemeData(
+        brightness: Brightness.light,
         primarySwatch: Colors.indigo,
         appBarTheme: const AppBarTheme(
           elevation: 0,
@@ -38,8 +50,6 @@ class CountryExplorerApp extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        // FINAL FIX: Use CardThemeData to configure card appearance
-        // This ensures the correct type is assigned for this SDK.
         cardTheme: CardThemeData(
           elevation: 4,
           shape: RoundedRectangleBorder(
@@ -48,8 +58,31 @@ class CountryExplorerApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.indigo,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        cardTheme: CardThemeData(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: _mode,
       // Home is set to AppWrapper which contains the BottomNavigationBar
-      home: const AppWrapper(),
+      home: AppWrapper(
+        onToggleTheme: _toggleTheme,
+        isDark: _mode == ThemeMode.dark,
+      ),
     );
   }
 }
